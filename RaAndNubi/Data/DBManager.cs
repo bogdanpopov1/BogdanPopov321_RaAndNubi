84a2bd989b97481b1dc4afedea2c66d6f3e18468
Богдан Попов <bogdanpopov5570@gmail.com>
2024-10-15 22:50:11 +0300

    Вывод пользователей в комбо бокс (авторизация)

---
+++ RaAndNubi/Data/DBManager.cs
@@ -0,0 +1,37 @@
+﻿using RaAndNubi.Data.DBO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaAndNubi.Data
+{
+    public static class DBManager
+    {
+        private static RaAndNubiEntities _context = new RaAndNubiEntities();
+        public static RaAndNubiEntities GetContext()
+        {
+            if (_context == null)
+                _context = new RaAndNubiEntities();
+            return _context;
+        }
+        public static bool UpdateDatabase()
+        {
+            try
+            {
+                _context.SaveChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public static List<Person> GetPeople()
+        {
+            return _context.Person.ToList();
+        }
+    }
+}
--- RaAndNubi/Windows/AuthorizationWindow.xaml.cs
+++ RaAndNubi/Windows/AuthorizationWindow.xaml.cs
@@ -11,14 +11,19 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using RaAndNubi.Data.DBO;
+using RaAndNubi.Data;
 
 namespace RaAndNubi.Windows
 {
     public partial class AuthorizationWindow : Window
     {
+        private List<Person> _people;
         public AuthorizationWindow()
         {
             InitializeComponent();
+            _people = DBManager.GetPeople();
+            LoginCB.ItemsSource = _people;
         }
 
         private void LoginBtn_Click(object sender, RoutedEventArgs e)
